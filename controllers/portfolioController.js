eed111017a8957438f10231912fa42a439ba8f4e
sarvjeet <sarvjeetsinghyash@gmail.com>
2023-08-05 19:02:57 +0530

    Initial commit

---
+++ controllers/portfolioController.js
@@ -0,0 +1,17 @@
+const sendEmailController = (req,res) => {
+    try {
+        return res.status(200).send({
+            success:true,
+            message:"Message sent successfully!",
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success:false,
+            message:"send Email API Error",
+            error,
+        });
+    }
+};
+
+module.exports = { sendEmailController };
